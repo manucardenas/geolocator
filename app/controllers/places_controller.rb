114bbd4048a0c62f9d69bf83265f16f848f15b48
Manuela Cardenas <manuelacardenasj@gmail.com>
2018-08-27 15:04:59 -0400

    allow users to create places without address

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = Place.new(place_params)
+    @place.ip = request.ip
     if @place.save
       redirect_to places_path, notice: "#{@place.name} Created!"
       render :create
@@ -46,7 +47,7 @@ class PlacesController < ApplicationController
   private
 
   def place_params
-    params.require(:place).permit(:name, :street, :city, :state, :country)
+    params.require(:place).permit(:name, :street, :city, :state, :country, :latitude, :longitude)
   end
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,28 @@
 class Place < ApplicationRecord
   validates :name, presence: true
-  geocoded_by :address
-  after_validation :geocode 
+  geocoded_by :geocode_method
+  after_validation :geocode , if: :needs_geocode?
 
   def address
     [street, city, state, country].compact.join(', ')
   end
+
+  private
+
+  def geocode_method
+    if address.present?
+      address
+    else
+      ip
+  end
+end
+
+  def needs_geocode
+    if address.present?
+      true
+    else
+      latitude.nil? || longitude.nil?
+  end
+end
 
 end
