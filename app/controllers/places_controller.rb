ace1c22f527e72ed2ee26f8298ef22c4614ef043
Manuela Cardenas <manuelacardenasj@gmail.com>
2018-08-27 15:14:34 -0400

    Allow user to delete places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,10 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show, :destroy]
 
   def create
     @place = Place.new(place_params)
     @place.ip = request.ip
     if @place.save
-      redirect_to places_path, notice: "#{@place.name} Created!"
       render :create
     else
       redirect_to places_path, notice: @place.errors.join(', ')
@@ -41,10 +41,18 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find(params[:id])
+  end
+
+  def destroy
+    @place.destroy
+    redirect_to places_path, notice: "#{@place.name} deleted!"
   end
 
   private
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
 
   def place_params
     params.require(:place).permit(:name, :street, :city, :state, :country, :latitude, :longitude)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :places, only: [:index, :show, :create]
+  resources :places, only: [:index, :show, :create, :destroy]
   root 'places#index'
 end
